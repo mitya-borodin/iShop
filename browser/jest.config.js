1f6f967a22ae3662b56034ad2cc803e4feda15be
Dmitriy Borodin <dmitriy@borodin.site>
2020-06-17 22:10:43 +0300

    Доработал конфигурации тестипования.

--- browser/jest.config.js
+++ browser/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json",
+    },
+  },
   preset: "ts-jest",
+
   ...require("@snowpack/app-scripts-svelte/jest.config.js")(),
   moduleFileExtensions: ["ts", "js", "json", "svelte"],
 };
