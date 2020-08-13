343511e266fcfcedb27a130dd8262b294eefeb82
Dmitriy Borodin <dmitriy@borodin.site>
2020-08-13 23:39:48 +0300

    feat(frontend): :sparkles: Update Home page.

--- packages/frontend/src/pages/Home.svelte
+++ packages/frontend/src/pages/Home.svelte
@@ -6,20 +6,21 @@
   <header class="bg-gray-100 h-screen flex justify-center items-center flex-col text-3xl">
     <img src="/logo.svg" class="App-logo pointer-events-none m-4 h-64" alt="logo" />
     <div>
-      <h1>Home</h1>
+      <h1>This Home Page</h1>
       <ul>
         <li class="text-orange-500">
           <Link href="/products">Products</Link>
         </li>
         <li class="text-orange-500">
-          <Link href="/products/1111">Product (4444)</Link>
+          <Link href="/products/100">Product (100)</Link>
+        </li>
+        <li class="text-orange-500">
+          <Link href="/signIn">Sign In</Link>
+        </li>
+        <li class="text-orange-500">
+          <Link href="/system-admin">System Admin</Link>
         </li>
       </ul>
     </div>
-    <p class="text-orange-500 ">
-      Edit
-      <code>src/App.svelte</code>
-      and save to reload.
-    </p>
   </header>
 </div>
