9930a9f1dc4cf62a3accf0314ec71509196a9858
Borodin Dmitriy <dmitriy@borodin.site>
2020-08-29 16:09:06 +0300

    feat(frontend): :sparkles: Add new user

--- packages/frontend/src/stores/user/AddUserForm.ts
+++ packages/frontend/src/stores/user/AddUserForm.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { ValueObjectFormStore } from "@rtcts/browser";
 import { AddUser } from "@rtcts/ishop-shared";
 import type { AddUserData } from "@rtcts/ishop-shared";
-import { ValidationResult } from "@rtcts/isomorphic";
+import { ValidationResult, logTypeEnum } from "@rtcts/isomorphic";
 import { computed } from "mobx";
 import { UserRepository } from "./UserRepository";
+import { browserHistory } from "../../shared/browserHistory";
 
 export class AddUserFormStore extends ValueObjectFormStore<AddUser, AddUserData> {
   protected readonly repository: UserRepository;
@@ -19,9 +21,34 @@ export class AddUserFormStore extends ValueObjectFormStore<AddUser, AddUserData>
     return this.repository.validationResult;
   }
 
+  protected async changeForm(
+    form: AddUser,
+    change: AddUserData & { inputFiles?: File[] },
+  ): Promise<AddUser> {
+    const changedForm = await super.changeForm(form, change);
+
+    if (this.externalValidationResult.hasError) {
+      this.repository.validationResult = new ValidationResult(
+        this.repository.validationResult
+          .toValidation()
+          .filter(({ type }) => type !== logTypeEnum.error),
+      );
+    }
+
+    return changedForm;
+  }
+
   protected async submitForm(submit: AddUser): Promise<void> {
     if (submit.isInsert()) {
       await this.repository.signUp(submit.toObject(), true);
+    }
+
+    if (!this.isValid) {
+      this.showValidation();
+    }
+
+    if (this.isValid) {
+      browserHistory.back();
     }
   }
 }
