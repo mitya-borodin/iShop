abc7e29c82857a0f21759a77061c6fe94f75f290
Dmitriy Borodin <dmitriy@borodin.site>
2020-11-05 01:39:36 +0300

    fix(deployment):

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,36 +98,11 @@ resource "google_container_cluster" "primary" {
   enable_legacy_abac = true
 
   master_auth {
+    username = "${var.project}-user"
+
     client_certificate_config {
       issue_client_certificate = true
     }
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      export KUBECONFIG=$(pwd)/.kube/config && \
-      mkdir -p .kube && \
-      echo ${base64decode(google_container_cluster.primary.master_auth[0].client_certificate)}​​​​​​​​ | base64 -D > .kube/ca.crt && \
-      echo ${base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)}​​​​​​​​ | base64 -D > .kube/client.crt && \
-      echo ${base64decode(google_container_cluster.primary.master_auth[0].client_key)}​​​​​​​​ | base64 -D > .kube/client.key && \
-      kubectl config set-cluster default \
-       --server=https://${google_container_cluster.primary.endpoint}​​​​​​​​ \
-       --certificate-authority=.kube/ca.crt \
-       --embed-certs && \
-      kubectl config set-credentials default \
-       --certificate-authority=.kube/ca.crt \
-       --client-key=.kube/client.key \
-       --client-certificate=.kube/client.crt \
-       --embed-certs && \
-      kubectl config set-context default --cluster=default --user=default && \
-      kubectl config use-context default && \
-      echo '#!/bin/sh' > kubectl && \
-      echo 'KUBECONFIG=.kube/config kubectl "$@"' >> kubectl && \
-      chmod a+x ./kubectl && \
-      echo '#!/bin/sh' > helm && \
-      echo 'KUBECONFIG=.kube/config helm "$@"' >> helm && \
-      chmod a+x ./helm
-    EOT
   }
 }
 
@@ -186,4 +161,33 @@ resource "helm_release" "cert-manager" {
   version          = "1.0.4"
   create_namespace = true
   namespace        = "cert-manager"
+}
+
+resource "null_resource" "generate_local_kubectl" {
+  provisioner "local-exec" {
+    command = <<EOT
+      export KUBECONFIG=$(pwd)/.kube/config && \
+      mkdir -p .kube && \
+      echo ${google_container_cluster.primary.master_auth[0].cluster_ca_certificate}​​​​​​​​ | base64 -D > .kube/ca.crt && \
+      echo ${google_container_cluster.primary.master_auth[0].client_certificate}​​​​​​​​ | base64 -D > .kube/client.crt && \
+      echo ${google_container_cluster.primary.master_auth[0].client_key}​​​​​​​​ | base64 -D > .kube/client.key && \
+      kubectl config set-cluster ${google_container_cluster.primary.name} \
+        --server=https://${google_container_cluster.primary.endpoint}​​​​​​​​ \
+        --certificate-authority=.kube/ca.crt \
+        --embed-certs && \
+      kubectl config set-credentials ${google_container_cluster.primary.master_auth[0].username} \
+        --certificate-authority=.kube/ca.crt \
+        --client-certificate=.kube/client.crt \
+        --client-key=.kube/client.key \
+        --embed-certs && \
+      kubectl config set-context default-context --cluster=${google_container_cluster.primary.name} --user=${google_container_cluster.primary.master_auth[0].username} && \
+      kubectl config use-context default-context && \
+      echo '#!/bin/sh' > kubectl && \
+      echo 'KUBECONFIG=.kube/config kubectl "$@"' >> kubectl && \
+      chmod a+x ./kubectl && \
+      echo '#!/bin/sh' > helm && \
+      echo 'KUBECONFIG=.kube/config helm "$@"' >> helm && \
+      chmod a+x ./helm
+    EOT
+  }
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,5 @@
-project = "ecn-js"
-credentials_file = "../credentials/ecn-js-8a7c4d5d3f20.json"
+project = "e-com-node-js"
+credentials_file = "../credentials/e-com-node-js-8862c2f6b006.json"
 region = "europe-north1"
 zone = "europe-north1-b"
 gke_num_nodes = 1
